18a83892bf9edbc86a180ed251c9045fe57f4959
abbott <abbott@emakefun.com>
2019-08-09 11:59:27 +0800

    update the lesson and file

---
+++ Lesson/Lesson19-High Sensitivity Water Sensor/ArduinoIDE Demo/Water_Level_Sensor/Water_Level_Sensor.ino
@@ -0,0 +1,44 @@
+/***********************************************************************
+ *       __                                                          _
+ *      / /        _____  __    __  _          _   (_)   ________   | |
+ *     / /____   / _____) \ \  / / | |   __   | |  | |  (  ______)  | |_____
+ *    / / ___/  | |_____   \ \/ /  | |  /  \  | |  | |  | |______   |  ___  |
+ *   / /\ \     | |_____|   \  /   | | / /\ \ | |  | |  (_______ )  | |   | |
+ *  / /  \ \__  | |_____    / /    | |/ /  \ \| |  | |   ______| |  | |   | |
+ * /_/    \___\  \______)  /_/      \__/    \__/   |_|  (________)  |_|   |_|
+ *
+ *
+ * KeyWay Tech firmware
+ *
+ * Copyright (C) 2015-2020
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, in version 3.
+ * learn more you can see <http://www.gnu.org/licenses/>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * [Title] Arduino water depth detecte
+ * [Diagram]
+ *         Arduino PIN 0   ===================  Water Sensor
+*/
+
+int analogPin=A0;
+double temp,data;
+void setup() {
+    pinMode(analogPin,INPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    temp=(long)analogRead(analogPin);
+    data=(temp/650)*4;
+    Serial.print("the depth is:");
+    Serial.print(data);
+    Serial.println("cm");
+    delay(1000);
+}
